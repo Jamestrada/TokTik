f9943aa8ecbdfc68e0815fae6eb1d2b660d7645a
James Estrada <Jamestrada@users.noreply.github.com>
2021-03-19 23:07:07 -0600

    create scrollable feed

---
+++ TokTik/Models/PostModel.swift
@@ -0,0 +1,21 @@
+//
+//  PostModel.swift
+//  TokTik
+//
+//  Created by James Estrada on 3/19/21.
+//
+
+import Foundation
+
+struct PostModel {
+    let identifier: String
+    
+    static func mockModels() -> [PostModel] {
+        var posts = [PostModel]()
+        for _ in 0...100 {
+            let post = PostModel(identifier: UUID().uuidString)
+            posts.append(post)
+        }
+        return posts
+    }
+}
