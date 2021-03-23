f5608720a42043a153b47774c35d29e40dd77ea4
James Estrada <Jamestrada@users.noreply.github.com>
2021-03-22 22:00:13 -0600

    add captions and double tap to like

--- TokTik/Controllers/Other/PostViewController.swift
+++ TokTik/Controllers/Other/PostViewController.swift
@@ -35,6 +35,18 @@ class PostViewController: UIViewController {
         return button
     }()
     
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.text = "Check out this video! #fyp #foryou #foryoupage"
+        label.font = .systemFont(ofSize: 24)
+        label.textColor = .white
+        return label
+    }()
+    
+    // MARK: - Init
+    
     init(model: PostModel) {
         self.model = model
         super.init(nibName: nil, bundle: nil)
@@ -51,6 +63,8 @@ class PostViewController: UIViewController {
         view.backgroundColor = colors.randomElement()
         
         setUpButtons()
+        setUpDoubleTapToLike()
+        view.addSubview(captionLabel)
     }
     
     override func viewDidLayoutSubviews() {
@@ -61,6 +75,13 @@ class PostViewController: UIViewController {
         for (index, button) in [likeButton, commentButton, shareButton].enumerated() {
             button.frame = CGRect(x: view.width - size - 10, y: yStart + (CGFloat(index) * 10) + (CGFloat(index) * size), width: size, height: size)
         }
+        
+        captionLabel.sizeToFit()
+        let labelSize = captionLabel.sizeThatFits(CGSize(width: view.width - size - 12, height: view.height))
+        captionLabel.frame = CGRect(x: 5,
+                                    y: view.height - 10 - view.safeAreaInsets.bottom - labelSize.height - (tabBarController?.tabBar.height ?? 0),
+                                    width: view.width - size - 12,
+                                    height: labelSize.height)
     }
     
     func setUpButtons() {
@@ -91,5 +112,44 @@ class PostViewController: UIViewController {
         
         let vc = UIActivityViewController(activityItems: [url], applicationActivities: [])
         present(vc, animated: true)
+    }
+    
+    func setUpDoubleTapToLike() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didDoubleTap(_:)))
+        tap.numberOfTapsRequired = 2
+        view.addGestureRecognizer(tap)
+        view.isUserInteractionEnabled = true
+    }
+    
+    @objc private func didDoubleTap(_ gesture: UITapGestureRecognizer) {
+        if !model.isLikedByCurrentUser {
+            model.isLikedByCurrentUser = true
+        }
+        
+        // show animation when double tap occurs
+        let touchPoint = gesture.location(in: view)
+        let imageView = UIImageView(image: UIImage(systemName: "heart.fill"))
+        imageView.tintColor = .systemRed
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        imageView.center = touchPoint
+        imageView.contentMode = .scaleAspectFit
+        imageView.alpha = 0
+        view.addSubview(imageView)
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            imageView.alpha = 1
+        }, completion: { done in
+            if done {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+                    UIView.animate(withDuration: 0.3, animations: {
+                        imageView.alpha = 0
+                    }, completion: { done in
+                        if done {
+                            imageView.removeFromSuperview()
+                        }
+                    })
+                })
+            }
+        })
     }
 }
