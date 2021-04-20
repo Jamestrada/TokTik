7ec31dc0e9f04dfbecb19214bd30a58286e5a0f0
James Estrada <Jamestrada@users.noreply.github.com>
2021-04-19 19:40:19 -0600

    add different types of notifications

---
+++ TokTik/Views/Other/Notifications/NotificationsPostCommentTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  NotificationsPostCommentTableViewCell.swift
+//  TokTik
+//
+//  Created by James Estrada on 4/19/21.
+//
+
+import UIKit
+
+class NotificationsPostCommentTableViewCell: UITableViewCell {
+    static let identifier = "NotificationsPostCommentTableViewCell"
+    
+    private let postThumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.textColor = .label
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.clipsToBounds = true
+        contentView.addSubview(postThumbnailImageView)
+        contentView.addSubview(label)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        postThumbnailImageView.image = nil
+        label.text = nil
+    }
+    
+    func configure(with postFileName: String) {
+        postThumbnailImageView.image = nil
+        label.text = nil
+    }
+}
---
+++ TokTik/Views/Other/Notifications/NotificationsUserFollowTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  NotificationsUserFollowTableViewCell.swift
+//  TokTik
+//
+//  Created by James Estrada on 4/19/21.
+//
+
+import UIKit
+
+class NotificationsUserFollowTableViewCell: UITableViewCell {
+    static let identifier = "NotificationsUserFollowTableViewCell"
+    
+    // avatar, label, follow button
+    
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.textColor = .label
+        return label
+    }()
+    
+    private let followButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Follow", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.clipsToBounds = true
+        contentView.addSubview(avatarImageView)
+        contentView.addSubview(label)
+        contentView.addSubview(followButton)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        avatarImageView.image = nil
+        label.text = nil
+    }
+    
+    func configure(with username: String) {
+        avatarImageView.image = nil
+        label.text = nil
+    }
+}
